63630a2ce97799c2736f8fe30acfb24a897677d2
Daniel Odeny <daniel.odeny@student.moringaschool.com>
2023-06-14 13:48:21 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity1, activity2 = "roller-skate!") {
+  if (!!activity1) {
+    return `This Saturday, I want to ${activity1}!`;
+  } else {
+    return `This Saturday, I want to ${activity2}`;
+  }
+}
+
+const mondayWork = function (activity1, activity2 = "go to the office") {
+  if (!!activity1) {
+    return `This Monday, I will ${activity1}.`;
+  } else {
+    return `This Monday, I will ${activity2}.`;
+  }
+};
+
+const wrapAdjective = function (visualFlair = "*") {
+  return function (adjective = "special") {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+};
